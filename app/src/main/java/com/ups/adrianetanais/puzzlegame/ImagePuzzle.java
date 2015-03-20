0273c46e214b4a5ac3d089e02ef4d9483a49b07b
Milila <cannac.anais@hotmail.fr>
2015-03-20 07:53:09 +0100

    Détecte fin de partie

--- app/src/main/java/com/ups/adrianetanais/puzzlegame/ImagePuzzle.java
+++ app/src/main/java/com/ups/adrianetanais/puzzlegame/ImagePuzzle.java
@@ -7,16 +7,22 @@ public class ImagePuzzle {
     protected int x;
     protected int y;
     protected Bitmap bitmap;
+    protected boolean fixed;
+    protected int xFinal;
+    protected int yFinal;
 
     ImagePuzzle(Bitmap bitmap) {
         this.bitmap = bitmap;
         this.x = 0;
         this.y = 0;
     }
-    ImagePuzzle(Bitmap bitmap, int x, int y) {
+    ImagePuzzle(Bitmap bitmap, int x, int y, boolean isFixed, int xFinal, int yFinal) {
         this.bitmap = bitmap;
         this.x = x;
         this.y = y;
+        this.fixed = isFixed;
+        this.xFinal = xFinal;
+        this.yFinal = yFinal;
     }
 
     public int getY() {
@@ -41,5 +47,19 @@ public class ImagePuzzle {
 
     public void setBitmap(Bitmap bitmap) {
         this.bitmap = bitmap;
+    }
+
+    public boolean isFixed(){
+        return this.fixed;
+    }
+    public void setFixed (boolean bool){
+        this.fixed = bool;
+    }
+    public int getYFinal() {
+        return yFinal;
+    }
+
+    public int getXFinal() {
+        return xFinal;
     }
 }
