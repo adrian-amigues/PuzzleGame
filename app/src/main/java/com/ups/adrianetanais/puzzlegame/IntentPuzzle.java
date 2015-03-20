09de5347baa03b9444fe6b43eafea32b6b014871
Adrian <adrian.amigues@gmail.com>
2015-03-20 19:40:35 +0100

    Apres merge, encore

--- app/src/main/java/com/ups/adrianetanais/puzzlegame/IntentPuzzle.java
+++ app/src/main/java/com/ups/adrianetanais/puzzlegame/IntentPuzzle.java
@@ -1,13 +1,8 @@
 package com.ups.adrianetanais.puzzlegame;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.Toast;
 
-/**
- * Created by Anaïs on 20/03/2015.
- */
 public class IntentPuzzle extends ActionBarActivity {
     @Override
     public void onCreate (Bundle savedInstanceState) {
@@ -15,5 +10,4 @@ public class IntentPuzzle extends ActionBarActivity {
         PlateauPuzzle plateau = new PlateauPuzzle(this);
         setContentView(plateau);
     }
-
 }
